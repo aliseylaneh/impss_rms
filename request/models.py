afc3270714556c94e4dd61c932b61b2a3b4cb40d
aliseylaneh <aliseylaneh@gmail.com>
2023-05-23 22:57:49 +0330

    requirements.txt and model.py request has been added

--- request/models.py
+++ request/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django_fsm import FSMField
+from common.db.basic import Logged
 
-# Create your models here.
+
+class RequestStateChoices(models.TextChoices):
+    STARTING = "ST", _("Starting")
+
+    COMPLETED = "CO", _("Completed")
+
+
+class Request(Logged):
+    provincial_organization = models.PositiveIntegerField(_("Provincial Organization"), null=False)
+    provincial_prison = models.PositiveIntegerField(_("Provincial Prison"), null=False)
+    state = FSMField(choices=RequestStateChoices.choices, default=RequestStateChoices.STARTING)
