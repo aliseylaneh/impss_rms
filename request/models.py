c0072bc4b147d354eae37a4a5810c7b18846254b
aliseylaneh <aliseylaneh@gmail.com>
2023-05-25 15:49:24 +0330

    requirment.txt updated and removed nonessential packages

--- request/models.py
+++ request/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django_fsm import FSMField
 from common.db.basic import Logged
+from django.utils.translation import gettext as _
 
 
 class RequestStateChoices(models.TextChoices):
