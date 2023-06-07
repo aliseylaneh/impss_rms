aab2010d6eba1b0cd82642109e0b07c2d3eab8f2
aliseylaneh <aliseylaneh@gmail.com>
2023-06-07 14:57:03 +0330

    added suitable django-fsm-admin and models.py

--- request/models.py
+++ request/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django_fsm import FSMField
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 
 
 class RequestStateChoices(models.TextChoices):
