c3d24ac18c2f8f09f75e5d70fb15332a14f17da7
aliseylaneh <aliseylaneh@gmail.com>
2023-06-07 13:46:18 +0330

    major changes plus docker file

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
+RUN pip install --upgrade pip setuptools wheel
 WORKDIR /rms
 COPY requirements.txt /rms/
 RUN pip install -r requirements.txt
--- request/models.py
+++ request/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django_fsm import FSMField
-from common.db.basic import Logged
 from django.utils.translation import gettext_lazy as _
 
 
@@ -10,7 +9,7 @@ class RequestStateChoices(models.TextChoices):
     COMPLETED = "CO", _("Completed")
 
 
-class Request(Logged):
+class Request(models.Model):
     provincial_organization = models.PositiveIntegerField(_("Provincial Organization"), null=False)
     provincial_prison = models.PositiveIntegerField(_("Provincial Prison"), null=False)
     state = FSMField(choices=RequestStateChoices.choices, default=RequestStateChoices.STARTING)
